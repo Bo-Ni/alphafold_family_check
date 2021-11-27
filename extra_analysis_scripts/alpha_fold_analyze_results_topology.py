1f35cf7609816e68638d560efe27fb09cc015e80
exsto <46631284+exsto1@users.noreply.github.com>
2021-11-27 13:31:55 +0100

    .

--- extra_analysis_scripts/alpha_fold_analyze_results_topology.py
+++ extra_analysis_scripts/alpha_fold_analyze_results_topology.py
@@ -1,15 +1,13 @@
 from alpha_fold_support_scripts import DownloadAlphaFoldSequences
-from alpha_fold_analyze_results_families import get_non_human_knot_info, get_human_knot_info
 
-knot_list = get_human_knot_info()
-knot_list.extend(get_non_human_knot_info())
-
-uni_ids = knot_list = [i[0] for i in knot_list]
+data_h = open("../info/summary2_filtered.txt")
+data = data_h.readlines()
+data_h.close()
+uni_ids = [i.split("---")[1].split("_")[0] for i in data if "---" in i]
 unique_ids = []
 for i in uni_ids:
-    if i not in unique_ids:
+    if uni_ids not in unique_ids:
         unique_ids.append(i)
 
-DownloadAlphaFoldSequences(unique_ids, "../temp_files/full_alignment.fasta")
-
 print(unique_ids)
+DownloadAlphaFoldSequences(unique_ids, "../temp_files/full_alignment_filtered.fasta")
